ce4cc571259f1e255a0fb0d54a5583db12c6855b
Giovanny Gabriel <giovanny.gabrieldsn@gmail.com>
2023-02-14 09:43:19 -0300

    adicionando o loading

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View, ActivityIndicator} from "react-native";
 import api from "./services/api";
 import Films from "./src/Films";
 export default function App() {
   const [films, setFilms] = useState([]);
-
+  const [loading,setLoading]= useState(true)
   useEffect(() => {
     async function loadFilms() {
       const response = await api.get("r-api/?api=filmes");
       setFilms(response.data);
+      setLoading(false)
     }
     loadFilms();
   }, []);
+  if(loading){
+    return(
+      <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+      <ActivityIndicator size={45} color="#121212"/>
+      </View>
+    )
+  }
   return (
     <View style={styles.container}>
       <FlatList
